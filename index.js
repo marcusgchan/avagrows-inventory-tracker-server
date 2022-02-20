357eeb7cbec205f362c0b1c72440eb8d8aee5989
Marcus Chan <marcusgchan35@gmail.com>
2022-02-19 16:06:58 -0800

    added build to gitignore

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("build"));
 
 app.use("/api/users", usersRouter);
 
