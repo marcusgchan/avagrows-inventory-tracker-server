6f2756f897db6016cc68282797f969e26fba1671
Ross Forster <rforster@sfu.ca>
2022-03-10 15:52:53 -0800

    small change to change location router-ross,aryan

--- routes/changeLocationQuatityRouter.js
+++ routes/changeLocationQuatityRouter.js
@@ -2,7 +2,7 @@ const changeLocationQuatityRouter = require("express").Router();
 const pool = require("../db");
 
 changeLocationQuatityRouter.post("/", (req, res) => {
-    var internal_part_number=req.body;
+    var internal_part_number;
     var oldLocation;
     var oldStatus;
     var oldQuantity;
