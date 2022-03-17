26e0d7d79848e148ed7119f937b5e9b755efb90e
HinFen Kong <hfk1002@gmail.com>
2022-03-16 23:38:06 -0700

    changed the name of partsRouter.js to rows.js

--- routes/addPartRouter.js
+++ routes/addPartRouter.js
@@ -1,25 +1,23 @@
 const addPartQuantityRouter = require("express").Router();
 const pool = require("../db");
+
 //add part to part quantity table
 addPartQuantityRouter.post("/", async (req, res) => {
-  var internal_part_number = req.body.internal_part_number;
-  var location_id = req.body.location_id;
-  var status_id = req.body.status_id;
-  var quantity = req.body.quantity;
-  var note = req.body.note;
+  let { internal_part_number, location_id, status_id, quantity, note } =
+    req.body;
   var results;
   var resultsForEntry;
   var resultsForTotalQuantity;
   var totalQuantity;
   //query to insert into parts quantity table
   var addNewPartQuantity = `INSERT INTO part_quantity values('${internal_part_number}', ${location_id}, ${status_id}, ${quantity}, '${note}';`;
- //query to check for dublicates in table
+  //query to check for dublicates in table
   var checkForDuplicates = `SELECT * FROM part_quantity WHERE internal_part_number = ${internal_part_number} AND status_id = ${status_id} AND location_id = ${location_id};`;
   //query to check parts table for entry
   var checkForEntry = `SELECT * FROM parts WHERE internal_part_number = ${internal_part_number};`;
 
   try {
-      //first check the parts table for entry
+    //first check the parts table for entry
     const result = await pool.query(checkForEntry);
     resultsForEntry = result.rows;
     resultsForTotalQuantity = result.rows[0].total_quantity;
@@ -32,7 +30,7 @@ addPartQuantityRouter.post("/", async (req, res) => {
     const result2 = await pool.query(checkForDuplicates);
 
     results = result2.rows;
-    
+
     //if no entry create a new entry and update total quantity
     if (results.length == 0) {
       await pool.query(addNewPartQuantity);
@@ -41,7 +39,8 @@ addPartQuantityRouter.post("/", async (req, res) => {
       //update the total quantity in the parts table
       var addNewPartTotalQuantity = `UPDATE parts SET total_quantity =${totalQuantity} WHERE internal_part_number = ${internal_part_number}`;
       await pool.query(addNewPartTotalQuantity);
-    } else { //if part is already in table then return
+    } else {
+      //if part is already in table then return
       console.log(
         "This part already exists at this location with this status!"
       );
--- routes/rowsRouter.js
+++ routes/rowsRouter.js
@@ -1,10 +1,10 @@
-const partsRouter = require("express").Router();
+const rowsRouter = require("express").Router();
 const pool = require("../db");
 
-partsRouter.get("/", (req, res) => {
-  var partsTableQuery =
+rowsRouter.get("/", (req, res) => {
+  var rowsTableQuery =
     "SELECT parts.internal_part_number, parts.part_name, locations.location_name, part_categories.part_category_name, statuses.status_name, part_quantity.quantity, part_quantity.serial, parts.total_quantity FROM parts INNER JOIN part_quantity ON parts.internal_part_number = part_quantity.internal_part_number INNER JOIN locations ON part_quantity.location_id = locations.location_id INNER JOIN part_categories ON parts.internal_part_number = part_categories.part_id INNER JOIN statuses ON part_quantity.status_id = statuses.status_id;";
-  pool.query(partsTableQuery, (error, result) => {
+  pool.query(rowsTableQuery, (error, result) => {
     if (error) {
       console.log(error);
       return;
@@ -14,4 +14,4 @@ partsRouter.get("/", (req, res) => {
   });
 });
 
-module.exports = partsRouter;
+module.exports = rowsRouter;
