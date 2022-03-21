fc852fe2d330587de55b95ee8fb174d4128dbaa1
HinFen Kong <hfk1002@gmail.com>
2022-03-21 11:12:02 -0700

    changed deletePartsRouter.js to return all the updated rows after succesful delete

--- routes/deletePartsRouter.js
+++ routes/deletePartsRouter.js
@@ -18,9 +18,13 @@ deletePartsRouter.post("/", async (req, res) => {
     var addStatusTableQuery = `DELETE FROM Parts WHERE internal_part_number = ${internal_part_number};`;
 
     await pool.query(addStatusTableQuery);
+
+    let rowResults = await pool.query(
+      `SELECT parts.internal_part_number, parts.part_name, locations.location_name, part_categories.part_category_name, statuses.status_name, part_quantity.quantity, part_quantity.serial, parts.total_quantity FROM parts INNER JOIN part_quantity ON parts.internal_part_number = part_quantity.internal_part_number INNER JOIN locations ON part_quantity.location_id = locations.location_id INNER JOIN part_categories ON parts.internal_part_number = part_categories.part_id INNER JOIN statuses ON part_quantity.status_id = statuses.status_id;`
+    );
+    res.status(200).json(rowResults.rows);
   } catch (e) {
-    console.log(e);
-    return;
+    res.status(400).json(e);
   }
 });
 
