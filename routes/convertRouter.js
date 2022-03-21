08894a65352b909aae588ba84a0869a3cf0e2f66
David Choi <daviddasol1@outlook.com>
2022-03-21 13:34:56 -0700

    added convert and unconvert routers and fixed moveLocationRouter

---
+++ routes/convertRouter.js
@@ -0,0 +1,8 @@
+const convertRouter = require("express").Router();
+const pool = require("../db");
+
+// change location of items
+convertRouter.post("/", async (req, res) => {
+});
+
+module.exports = convertRouter;
---
+++ routes/unconvertRouter.js
@@ -0,0 +1,8 @@
+const unconvertRouter = require("express").Router();
+const pool = require("../db");
+
+// change location of items
+unconvertRouter.post("/", async (req, res) => {
+});
+
+module.exports = unconvertRouter;
