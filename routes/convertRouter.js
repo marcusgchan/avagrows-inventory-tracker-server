702610bb675c7802b0028b4686d1b37c4a76e3c6
David Choi <daviddasol1@outlook.com>
2022-03-21 13:59:49 -0700

    changed req.body attributes for convert and uncovert

--- routes/convertRouter.js
+++ routes/convertRouter.js
@@ -3,6 +3,15 @@ const pool = require("../db");
 
 // change location of items
 convertRouter.post("/", async (req, res) => {
+   let {
+        internal_part_number,
+        quantity,
+        location_id,
+        status_id,
+        new_location_id,
+        new_status_id,
+        convertQty,
+      } = req.body;
 });
 
 module.exports = convertRouter;
